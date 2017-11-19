72da81dfa59b96c62214201425ff8e005f812504
Ilan Cohen <ilan@chegg.com>
2017-11-19 11:44:11 +0200

    More styling, all content

--- js/main.js
+++ js/main.js
@@ -1,8 +1,22 @@
-window.document.addEventListener('load', function(){
-    setHeight();
-});
-setHeight();
+window.document.addEventListener('load', setCover);
+window.document.addEventListener('resize', setCover);
+setCover();
 
-function setHeight(){
-    document.querySelector('.bg').style.height = document.querySelector('.parallax').scrollHeight + 'px';
+function setCover(){
+    var parallax = document.querySelector('.parallax');
+    var bg = document.querySelector('.bg');
+    bg.style.height = parallax.scrollHeight + 'px';
+    bg.style.width = parallax.offsetWidth * 1.5 + 'px';
+}
+
+var nav_links = document.querySelectorAll('nav a');
+for(var i = 0; i < nav_links.length; i++) {
+    nav_links[i].addEventListener('click', function(event) {\
+        event.preventDefault();
+        var link = event.target.hash;
+        document.querySelector(link).scrollIntoView({
+            behavior: 'smooth'
+        });
+        return false;
+    });
 }
